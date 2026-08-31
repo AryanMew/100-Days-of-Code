0946e8043f539fd8333cade11f1e32a67d15a97c
Aryan M. <aryanmewani@gmail.com>
2026-08-31 12:00:00 +0530

    Day 22

---
+++ Day 22/DAY22P1.c
@@ -0,0 +1,34 @@
+// Q43: Write a program to check if a number is a strong number.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n, original, digit, i, fact, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        digit = n % 10;
+        fact = 1;
+
+        for (i = 1; i <= digit; i++)
+        {
+            fact = fact * i;
+        }
+
+        sum = sum + fact;
+        n = n / 10;
+    }
+
+    if (sum == original)
+        printf("Strong number");
+    else
+        printf("Not strong number");
+
+    return 0;
+}
