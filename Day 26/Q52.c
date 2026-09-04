e2bfcd88aa8c8db4c0d937b9390d4c198315f218
Aryan M. <aryanmewani@gmail.com>
2026-09-04 12:00:00 +0530

    Day 26

---
+++ Day 26/Q52.c
@@ -0,0 +1,47 @@
+/*
+Q52: Write a program to print the following pattern:
+
+*
+ 
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i += 2)
+    {
+        for (j = 1; j <= i; j++)
+            printf("*\n");
+
+        printf("\n");
+    }
+
+    for (i = 3; i >= 1; i -= 2)
+    {
+        for (j = 1; j <= i; j++)
+            printf("*\n");
+
+        if (i != 1)
+            printf("\n");
+    }
+
+    return 0;
+}
