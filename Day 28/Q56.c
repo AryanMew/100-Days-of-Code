de179f134678334cc027cc29516498b2f4c87377
Aryan M. <aryanmewani@gmail.com>
2026-09-06 18:00:00 +0530

    Day 28

---
+++ Day 28/Q56.c
@@ -0,0 +1,24 @@
+// Q56: Read and print elements of a one-dimensional array.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, a[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
