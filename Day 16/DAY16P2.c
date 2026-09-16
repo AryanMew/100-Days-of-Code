a03aa6995efe58d44205a122a7b43606af1ca361
Aryan M. <aryanmewani@gmail.com>
2026-09-16 12:32:15 +0530

    Day 16

---
+++ Day 16/DAY16P2.c
@@ -0,0 +1,27 @@
+// Q32: Write a program to check if a number is a palindrome.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n, original, reverse = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
