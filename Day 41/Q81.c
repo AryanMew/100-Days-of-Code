3cca194622d4d687f72281fccb1963d06664a031
Aryan M. <aryanmewani@gmail.com>
2026-09-19 12:00:00 +0530

    Day 41

---
+++ Day 41/Q81.c
@@ -0,0 +1,22 @@
+// Q81: Count characters in a string without using built-in length functions.
+
+#include <stdio.h>
+
+int main(void)
+{
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != '\n')
+            count++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
